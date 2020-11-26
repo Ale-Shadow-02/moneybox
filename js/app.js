912856ff1d20c216efa7f8156a77200cb32ad347
boris Baganov <bbaganov@gmail.com>
2020-11-26 19:02:19 +0300

    1

--- js/app.js
+++ js/app.js
@@ -41,3 +41,73 @@ formDelete.forEach(el => {
 });
 
 
+    let nowDate = Date.now();
+    console.log((nowDate/86400000)/365);
+    let valueData = Date.parse(2021-07-22);
+    console.log((valueData/86400000)/365);
+
+    let  currentSek = valueData;
+
+    let currentDay = currentSek/86400000
+console.log(currentDay/365);
+
+startMoney = 2000;
+moneyAmount1 = 1000;
+moneyAmount2 = 1000;
+moneyAmount3 = 1000;
+moneyAmount4 = 1000;
+monthAmount = 5;
+
+for(let i = 1; i <= monthAmount; i++){
+    startMoney = startMoney *1.00417
+}
+monthAmount--;
+
+for(let i = 1; i <= monthAmount; i++){
+    moneyAmount1 = moneyAmount1 *1.00417
+}
+
+monthAmount--;
+for(let i = 1; i <= monthAmount; i++){
+    moneyAmount2 = moneyAmount2 *1.00417
+}
+monthAmount--;
+for(let i = 1; i <= monthAmount; i++){
+    moneyAmount3 = moneyAmount3 *1.00417
+}
+monthAmount--;
+for(let i = 1; i <= monthAmount; i++){
+    moneyAmount4 = moneyAmount4 *1.00417
+}
+monthAmount--;
+console.log(startMoney);
+console.log(moneyAmount1);
+console.log(moneyAmount2);
+console.log(moneyAmount3);
+console.log(moneyAmount4);
+console.log(moneyAmount2 + moneyAmount1 + startMoney + moneyAmount3 +moneyAmount4);
+
+
+let summ = 0;
+const need = 1000000;
+let have = 200000;
+let have1 = have;
+let mon = 18;
+let per = 1.00417;
+for(let j =1; j < mon; j++){
+    have1*=per;
+}
+let pay = (((need*0.98) - have1))/mon;
+let current = pay;
+console.log('month', pay)
+ for(let i=1; i<mon; i++ ){
+     have *= per;
+     current = current*per;
+     console.log('cur', current)
+     current = current + pay;
+     console.log('cur1', current)   
+ }
+ summ = have + current;
+ console.log('sum', summ);
+ console.log('have', have);
+ console.log('current', current);
