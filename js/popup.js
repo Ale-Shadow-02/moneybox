7251e090bb190200a3e9962af33081ea1b0e25d1
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2020-11-28 13:43:28 +0400

    add modal-window & add popup.js & add popup css

--- js/popup.js
+++ js/popup.js
@@ -1,126 +1,26 @@
-/* const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock-padding");
+let openPopup = document.querySelector('.link-open');
+let popup = document.querySelector('.popup');
+let closePopup = document.querySelector('.close-popup');
+let popupBody = document.querySelector('.popup__body');
 
+openPopup.addEventListener('click', (e) => {
+  e.preventDefault();
+  popup.classList.add('open');
+});
 
-let unlock = true;
+closePopup.addEventListener('click', (e) => {
+  e.preventDefault();
+  popup.classList.remove('open');
+});
 
-const timeout = 800;
-
-if (popupLinks.length > 0) {
-  for (let index = 0; index < popupLinks.length; index++) {
-    const popupLink = popupLinks[index];
-    popupLink.addEventListener("click", function (e) {
-      const popupName = popupLink.getAttribute('href').replace('#', '');
-      const curentPopup = document.getElementById(popupName);
-      popupOpen(curentPopup);
-      e.preventDefault();
-    });
-  }
-}
-const popupCloseIcon = document.querySelectorAll('.close-popup');
-if (popupCloseIcon.length > 0) {
-  for (let index = 0; index < popupCloseIcon.length; index++) {
-    const el = popupCloseIcon[index];
-    el.addEventListener('click', function (e) {
-      popupClose(el.closest('.popup'));
-      e.preventDefault();
-    });
-  }
-}
-
-function popupOpen(curentPopup) {
-  if (curentPopup && unlock) {
-    const popupActive = document.querySelector('.popup.open');
-    if (popupActive) {
-      popupClose(popupActive, false);
-    } else {
-      bodyLock();
-    }
-    curentPopup.classList.add('open');
-    curentPopup.addEventListener("click", function (e) {
-      if (!e.target.closest('.popup__content')) {
-        popupClose(e.target.closest('.popup'));
-      }
-    });
-  }
-}
-
-function popupClose(popupActive, doUnlock = true) {
-  if (unlock) {
-    popupActive.classList.remove('open');
-    if (doUnlock) {
-      bodyUnLock();
-    }
-  }
-}
-
-function bodyLock() {
-  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-
-  if (lockPadding.length > 0) {
-    for (let index = 0; index < lockPadding.length; index++) {
-      const el = lockPadding[index];
-      el.style.paddingRight = lockPaddingValue;
-    }
-  }
-  body.style.paddingRight = lockPaddingValue;
-  body.classList.add('lock');
-
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout);
-}
-
-function bodyUnLock() {
-  setTimeout(function () {
-    if (lockPadding.length > 0) {
-      for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = '0px';
-      }
-    }
-    body.style.paddingRight = '0px';
-    body.classList.remove('lock');
-  }, timeout);
-
-  unlock = false;
-  setTimeout(function () {
-    unlock = true;
-  }, timeout);
-}
-
-document.addEventListener('keydown', function (e) {
-  if (e.which === 27) {
-    const popupActive = document.querySelector('.popup.open');
-    popupClose(popupActive);
+popupBody.addEventListener('click', (e) => {
+  if (!e.target.closest('.popup__content')) {
+    popup.classList.remove('open');
   }
 });
 
-(function () {
-  // проверяем поддержку
-  if (!Element.prototype.closest) {
-    // реализуем
-    Element.prototype.closest = function (css) {
-      var node = this;
-      while (node) {
-        if (node.matches(css)) return node;
-        else node = node.parentElement;
-      }
-      return null;
-    };
+document.addEventListener('keyup', (e) => {
+  if (e.keyCode === 27) {
+    popup.classList.remove('open');
   }
-})();
-(function () {
-  // проверяем поддержку
-  if (!Element.prototype.matches) {
-    // определяем свойство
-    Element.prototype.matches = Element.prototype.matchesSelector ||
-      Element.prototype.webkitMatchesSelector ||
-      Element.prototype.mozMatchesSelector ||
-      Element.prototype.msMatchesSelector;
-  }
-})(); */
-
-let openPopup = document.querySelector('.link-open');
+});
